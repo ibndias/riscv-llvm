bd5785073e30078756dc4b5cde7711fe75d3e086
Derry Pratama <derry@islab.re.kr>
2021-04-29 09:56:43 +0900

    Optimized mte mpk

--- llvm/lib/Target/RISCV/RISCVMachineInstrPrinter.cpp
+++ llvm/lib/Target/RISCV/RISCVMachineInstrPrinter.cpp
@@ -86,10 +86,10 @@ bool RISCVMachineInstrPrinter::runOnMachineFunction(MachineFunction &MF) {
               .addImm(0xC00)
               .addReg(RISCV::X0);
 
-          // csrrci x0, csr, imm
-          BuildMI(MBB, MI, DL, XII->get(RISCV::CSRRS), RISCV::X0)
-              .addImm(0xC00)
-              .addReg(RISCV::X0);
+          // // csrrci x0, csr, imm
+          // BuildMI(MBB, MI, DL, XII->get(RISCV::CSRRS), RISCV::X0)
+          //     .addImm(0xC00)
+          //     .addReg(RISCV::X0);
           // END_MPK
 
           // save ra to shadow : sw ra, 0(t6)
@@ -108,10 +108,10 @@ bool RISCVMachineInstrPrinter::runOnMachineFunction(MachineFunction &MF) {
           BuildMI(MBB, MI, DL, XII->get(RISCV::CSRRS), RISCV::X0)
               .addImm(0xC00)
               .addReg(RISCV::X0);
-          // csrrsi x0, csr, imm
-          BuildMI(MBB, MI, DL, XII->get(RISCV::CSRRS), RISCV::X0)
-              .addImm(0xC00)
-              .addReg(RISCV::X0);
+          // // csrrsi x0, csr, imm
+          // BuildMI(MBB, MI, DL, XII->get(RISCV::CSRRS), RISCV::X0)
+          //     .addImm(0xC00)
+          //     .addReg(RISCV::X0);
           // END_MPK
           is_instrumented = true;
         }
