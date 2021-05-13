6108f15641f33be46c654ca403c3e928c34ac616
Derry Pratama <derry@islab.re.kr>
2021-05-13 16:21:30 +0900

    remove accidentally added sw

--- llvm/lib/Target/RISCV/RISCVMachineInstrPrinter.cpp
+++ llvm/lib/Target/RISCV/RISCVMachineInstrPrinter.cpp
@@ -114,10 +114,6 @@ bool RISCVMachineInstrPrinter::runOnMachineFunction(MachineFunction &MF) {
 
       // add load instruction before every memory access instructions - Jinjae
       if (MI.mayLoadOrStore()) {
-        // save ra to shadow : sw ra, 0(t6)
-        BuildMI(MBB, MI, DL, XII->get(RISCV::SD), RISCV::X1)
-            .addReg(RISCV::X31)
-            .addImm(0);
         // load shadow to ra : lw ra, 0(t6)
         BuildMI(MBB, MI, DL, XII->get(RISCV::LD), RISCV::X1)
             .addReg(RISCV::X31)
