763ddebd9f1f335b6aed3095a0291f72fc5cef96
Derry Pratama <derry@islab.re.kr>
2021-05-14 09:45:37 +0900

    fix compile error

--- llvm/lib/Target/RISCV/RISCVMachineInstrPrinter.cpp
+++ llvm/lib/Target/RISCV/RISCVMachineInstrPrinter.cpp
@@ -115,13 +115,16 @@ bool RISCVMachineInstrPrinter::runOnMachineFunction(MachineFunction &MF) {
           // END_MPK
           is_instrumented = true;
         }
-      }
-      // add load instruction before every memory access instructions - Jinjae
-      if (MI.mayLoadOrStore()) {
-        // load shadow to ra : lw ra, 0(t6)
-        BuildMI(MBB, MI, DL, XII->get(RISCV::LD), RISCV::X1)
-            .addReg(RISCV::X31)
-            .addImm(0);
+      } else {
+
+        // add load instruction before every memory access instructions - Jinjae
+        if (MI.mayLoadOrStore()&& MF.getName() != "main") {
+          // load shadow to ra : lw ra, 0(t6)
+          BuildMI(MBB, MI, DL, XII->get(RISCV::LD), RISCV::X1)
+              .addReg(RISCV::X31)
+              .addImm(0);
+          is_instrumented = true;
+        }
       }
       if (MI.isReturn() && MF.getName() != "main") {
         // START_MPK
